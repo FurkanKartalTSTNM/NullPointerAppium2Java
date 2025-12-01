8676cb8b680ef66ded9e1a5c67c0926b17c11341
Furkan Kartal <Furkan.kartal@testinium.com>
2025-12-01 13:29:46 +0300

    deneme

--- src/test/java/com/testinium/IOSTest.java
+++ src/test/java/com/testinium/IOSTest.java
@@ -25,7 +25,7 @@ public class IOSTest extends BaseTest {
         tapAllLocationAlerts();
 
         // iOS butonu için locator
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement kategorilerButton = wait.until(
                 ExpectedConditions.elementToBeClickable(
                         AppiumBy.xpath("//XCUIElementTypeButton[contains(@name, \"mainTabCategoriesTabBtn\")]")
